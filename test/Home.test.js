79e9914754da2df387d0d0fd0314c241123e1744
yang <nechinyoung@hotmail.com>
2022-08-13 17:44:51 +0800

    fix

--- test/Home.test.js
+++ test/Home.test.js
@@ -3,6 +3,6 @@ import { shallowMount } from "@vue/test-utils";
 
 test('text is right', () => {
   const wraper = shallowMount(Login)
-  expect(wraper.text()).toBe('Hello World!')
+  expect(wraper.text()).toBe('Login')
   expect(2 + 2).toBe(4)
 })
