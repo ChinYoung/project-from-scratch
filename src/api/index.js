13c8d4005976de724117ea74cdefa05b991e0eb4
ChinYoung <woodichin@outlook.com>
2022-09-02 15:25:06 +0800

    fix: port error

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
 const url = import.meta.env.VITE_URL
+const httpRequest = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL
+})
 const getToken = async (user) => {
-    const { data: res } = await axios.post(`${url}/account`, user)
+    const { data: res } = await httpRequest.post(`${url}/account`, user)
     return res
 }
 const getusers = async (timestamp, nonce, sig, token) => {
-    const { data: res } = await axios.get(`${url}/user?pageSize=10&pageNumber=0&timestamp=${timestamp}&nonce=${nonce}&sig=${sig}`, {
+    const { data: res } = await httpRequest.get(`${url}/user?pageSize=10&pageNumber=0&timestamp=${timestamp}&nonce=${nonce}&sig=${sig}`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
